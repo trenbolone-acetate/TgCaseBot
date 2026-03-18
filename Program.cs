7d77fae4c369e02b1e398279772e526f848034a1
cheez <cheezballz03@gmail.com>
2026-03-18 14:36:08 +0200

    Create test for bot functionality. Echoing user messages back.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿namespace TgCaseBot;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 class Program
-{
-    static void Main(string[] args)
+{ 
+    static TelegramBotClient bot = new TelegramBotClient(File.ReadAllText("../../../tkn.txt"));
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using var cts = new CancellationTokenSource();
+        var me = await bot.GetMe();
+        bot.OnMessage += OnMessage;
+        
+        Console.WriteLine($"bot @{me.Username} currently running..");
+        Console.ReadLine();
+        await cts.CancelAsync(); 
+    }
+    static async Task OnMessage(Message msg, UpdateType type)
+    {
+        if (msg.Text is null) return;
+        Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+        
+        await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
     }
 }
